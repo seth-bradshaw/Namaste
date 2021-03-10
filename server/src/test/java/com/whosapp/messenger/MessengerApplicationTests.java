b0b5f23ffdd1fc420c528b41e133f1381ac3600b
Seth Bradshaw <seth.bradshaw27@gmail.com>
2021-03-09 18:36:11 -0700

    got basic project initialized

---
+++ server/src/test/java/com/whosapp/messenger/MessengerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.whosapp.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
